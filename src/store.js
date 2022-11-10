c9377a4c227559ec6fd21c0c268d44c2c50ab42d
kunal-mandalia <kunal.v.mandalia@gmail.com>
2022-11-10 01:28:03 +0000

    fix: db conn

--- src/store.js
+++ src/store.js
@@ -41,9 +41,42 @@ class Store {
         transaction.objectStore(this.storeName).put(bullet);
     }
 
-    async getBullets() {
+    async getDBConnection() {
         return new Promise((resolve, reject) => {
-            const transaction = this.db.transaction([this.storeName], "readonly");
+            const request = this.indexedDB.open("whatsimportant", this.dbVersion);
+            request.onsuccess = (event) => {
+                console.log("Success creating/accessing IndexedDB database");
+                const db = request.result;
+
+                db.onerror = function (event) {
+                    console.log("Error creating/accessing IndexedDB database");
+                };
+
+                // Interim solution for Google Chrome to create an objectStore. Will be deprecated
+                if (this.db.setVersion) {
+                    if (db.version !== this.dbVersion) {
+                        var setVersion = db.setVersion(this.dbVersion);
+                        setVersion.onsuccess = function () {
+                            db.createObjectStore(this.db);
+                        };
+                    }
+                }
+                return resolve(db);
+            }
+            request.onupgradeneeded = (event) => {
+                const db = event.target.result;
+                console.log(`Upgrading to version ${db.version}`);
+                db.createObjectStore(this.storeName, { keyPath: 'id', autoIncrement: true });
+                return resolve(db);
+            };
+
+        })
+    }
+
+    async getBullets() {
+        return new Promise(async (resolve, reject) => {
+            const db = await this.getDBConnection();
+            const transaction = db.transaction([this.storeName], "readonly");
             const request = transaction.objectStore(this.storeName).getAll();
             request.onsuccess = (event) => {
                 return resolve(event.target.result);
