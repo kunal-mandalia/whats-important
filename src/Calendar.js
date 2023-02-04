146bc52f39b4cccff81965140d276579d8ed6666
kunal-mandalia <kunal.v.mandalia@gmail.com>
2023-02-04 23:37:10 +0000

    fix: CI

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,12 +1,10 @@
 import { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { PulseLoader } from 'react-spinners';
 import { CalendarSettings } from './Settings';
 import store from './store';
 
 
 export function Calendar({ isSmall }) {
-    // TODO get links from store
     const [loading, setLoading] = useState(true);
     const [links, setLinks] = useState({
         mobile: '',
@@ -28,13 +26,6 @@ export function Calendar({ isSmall }) {
     useEffect(() => {
         loadLinks();
     }, []);
-
-    async function handleSave() {
-        await store.saveCalendarLinks(links);
-    }
-
-    // const calendarSrc = isSmall ? "https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FLondon&showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=1&showTz=0&mode=AGENDA&title=What's%20Important&src=a3VuYWwudi5tYW5kYWxpYUBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udWsjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%237986CB&color=%2333B679&color=%230B8043"
-        // : "https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FLondon&showTitle=0&showNav=0&showDate=0&showPrint=0&showTabs=0&showCalendars=1&showTz=0&mode=WEEK&title=What's%20Important&src=a3VuYWwudi5tYW5kYWxpYUBnbWFpbC5jb20&src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&src=ZW4udWsjaG9saWRheUBncm91cC52LmNhbGVuZGFyLmdvb2dsZS5jb20&color=%237986CB&color=%2333B679&color=%230B8043"
 
     const calendarSrc = isSmall ? links.mobile : links.desktop;
 
